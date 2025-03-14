0eba727ad2e773c4a5b25cd829db37ca57204265
Mike7s <146244208+Mike7s@users.noreply.github.com>
2025-03-14 17:34:22 +0100

    Fix type in useFetch

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,24 +1,18 @@
 import { useState, useEffect } from 'react';
 import axios, { AxiosError, AxiosResponse } from 'axios';
 
-
-interface ApiResponse<T> {
-  data: T;
-}
-
 function useFetch<T>(url: string) {
-  const [data, setData] = useState<T | null>(null);
+  const [data, setData] = useState<T | null>(null);  
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    
     const fetchData = async () => {
       setLoading(true);
       setError(null);
       try {
         
-        const response: AxiosResponse<ApiResponse<T>> = await axios.get(url);
+        const response: AxiosResponse<T> = await axios.get(url);
         setData(response.data); 
       } catch (err) {
         const axiosError = err as AxiosError;
